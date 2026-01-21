2d49b31cc592fcf3e63cab4cbeaf3ec205df75a5
greenfirn <40929137+greenfirn@users.noreply.github.com>
2026-01-21 10:21:59 -0500

    6600XT ocs

--- gpu_apply_ocs - AMD 6600XT-warthog.sh
+++ gpu_apply_ocs - AMD 6600XT-warthog.sh
@@ -2,7 +2,7 @@ sudo tee /usr/local/bin/gpu_apply_ocs.sh > /dev/null <<'EOF'
 #!/bin/bash
 # Save as set-amd-clocks.sh
 
-echo "Setting AMD RX 6600 XT for mining..."
+echo "Setting AMD RX 6600 XT for mining... Warthog"
 
 # Set manual control
 echo "manual" | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level
@@ -10,16 +10,14 @@ echo "manual" | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance
 # Set core to 1950MHz (will throttle with power limit)
 echo "1" | sudo tee /sys/class/drm/card0/device/pp_dpm_sclk
 
-# Set memory to 1000MHz
-echo "3" | sudo tee /sys/class/drm/card0/device/pp_dpm_mclk
+# Set memory to 96MHz
+echo "0" | sudo tee /sys/class/drm/card0/device/pp_dpm_mclk
 
-# Set power limit to 69W (this controls actual speed)
-echo 69000000 | sudo tee /sys/class/drm/card0/device/hwmon/hwmon*/power1_cap
+# Set power limit to 42W (this controls actual speed)
+echo 42000000 | sudo tee /sys/class/drm/card0/device/hwmon/hwmon*/power1_cap
 
 # Set fan speed 70 %
 # echo 180 | sudo tee /sys/class/drm/card0/device/hwmon/hwmon*/pwm1
-
-echo "Done! Actual core speed will be ~1350MHz due to 69W power limit"
 
 # Check current states
 cat /sys/class/drm/card0/device/pp_dpm_sclk
@@ -30,4 +28,4 @@ EOF
 # make it executable
 sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
 
-# sudo /usr/local/bin/gpu_apply_ocs.sh
+sudo /usr/local/bin/gpu_apply_ocs.sh
