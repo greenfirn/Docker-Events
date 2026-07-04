39def5a8a57b94b9cbf770963f69f54e37b6f58e
greenfirn <40929137+greenfirn@users.noreply.github.com>
2026-07-04 07:08:49 -0400

    py-nvtool

---
+++ source/gpu_apply_ocs - 3090.sh
@@ -0,0 +1,13 @@
+sudo tee /usr/local/bin/gpu_apply_ocs.sh > /dev/null <<'EOF'
+#!/bin/bash
+echo "Setting 3090 for mining... Keryx"
+
+py-nvtool --setcoreoffset 150 --setmemoffset 600
+
+EOF
+
+# make it executable
+sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
+
+# manual test
+sudo /usr/local/bin/gpu_apply_ocs.sh
