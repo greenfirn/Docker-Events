bf27937597a02f3822c9af98101cbef6af23cb65
greenfirn <40929137+greenfirn@users.noreply.github.com>
2026-07-10 06:25:19 -0400

    Update gpu_apply_ocs - 4070TiS.sh

--- source/gpu_apply_ocs - 4070TiS.sh
+++ source/gpu_apply_ocs - 4070TiS.sh
@@ -13,3 +13,18 @@ sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
 
 # manual test
 sudo /usr/local/bin/gpu_apply_ocs.sh
+
+
+
+sudo tee /usr/local/bin/gpu_apply_ocs.sh > /dev/null <<'EOF'
+#!/bin/bash
+echo "Setting 4070TiS for mining... Keryx"
+
+py-nvtool --setcore 2400 --setcoreoffset 300 --setmemoffset 2000
+EOF
+
+# make it executable
+sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
+
+# manual test
+sudo /usr/local/bin/gpu_apply_ocs.sh
