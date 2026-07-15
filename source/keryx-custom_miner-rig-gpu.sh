4691fce413a19ec051add314977b626bf702d59a
Crossedwires <40929137+greenfirn@users.noreply.github.com>
2026-07-15 00:06:10 -0400

    Update keryx-custom_miner-rig-gpu.sh

--- source/keryx-custom_miner-rig-gpu.sh
+++ source/keryx-custom_miner-rig-gpu.sh
@@ -1,10 +1,11 @@
-sudo tee /home/user/rig-gpu.conf > /dev/null <<'EOF'
+tee /home/user/rig-gpu.conf > /dev/null <<'EOF'
 TARGET_IMAGE 0 ""
 TARGET_NAME 0 ""
 RESET_OC 0 "true"
 APPLY_OC 0 "true"
 SCREEN_NAME 0 "gpu"
-CUSTOM_MINER 0 "/home/user/miners/keryx-miner/keryx-miner"
+CUSTOM_MINER_URL 0 "https://github.com/Keryx-Labs/keryx-miner/releases/download/v0.3.6-OPoI/keryx-miner-v0.3.6-OPoI-linux-gnu-amd64.zip"
+CUSTOM_MINER 0 "keryx-miner"
 MINER 0 ""
 ALGO 0 ""
 POOL 0 ""
@@ -12,5 +13,6 @@ WALLET 0 ""
 PASS 0 ""
 ARGS 0 "--mining-address keryx:****************** --keryxd-address 10.20.0.105:22110"
 EOF
+
 sudo systemctl restart docker_events_gpu
 sudo systemctl is-active docker_events_gpu
