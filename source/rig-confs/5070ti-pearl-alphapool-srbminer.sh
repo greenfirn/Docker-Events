a2b2430225e90a670a8a1aae3d62a32d5feb1074
Crossedwires <40929137+greenfirn@users.noreply.github.com>
2026-07-15 18:17:45 -0400

    Add configuration script for 5070ti mining setup

---
+++ source/rig-confs/5070ti-pearl-alphapool-srbminer.sh
@@ -0,0 +1,15 @@
+tee /home/user/rig-gpu.conf > /dev/null <<'EOF'
+TARGET_IMAGE 0 ""
+TARGET_NAME 0 ""
+RESET_OC 0 "true"
+SCREEN_NAME 0 "gpu"
+CUSTOM_MINER 0 ""
+MINER 0 "srbminer"
+ALGO 0 "pearlhash"
+POOL 0 "us1.alphapool.tech:5571, us2.alphapool.tech:5571"
+WALLET 0 "prl1***********+mdl1*******************"
+PASS 0 "x"
+ARGS 0 "--worker %WORKER_NAME% --tls false --disable-cpu --gpu-id 0 --gpu-cclock0 2490 --gpu-coffset0 300 --gpu-mclock0 7001 --gpu-plimit0 300"
+EOF
+sudo systemctl restart docker_events_gpu
+sudo systemctl is-active docker_events_gpu
