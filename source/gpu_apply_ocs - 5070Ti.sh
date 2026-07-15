9556cd0e632597964c059490b4e4d4196b140bc9
Crossedwires <40929137+greenfirn@users.noreply.github.com>
2026-07-15 00:27:31 -0400

    Update gpu_apply_ocs - 5070Ti.sh

--- source/gpu_apply_ocs - 5070Ti.sh
+++ source/gpu_apply_ocs - 5070Ti.sh
@@ -14,18 +14,3 @@ sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
 # manual test
 sudo /usr/local/bin/gpu_apply_ocs.sh
 
-
-sudo tee /usr/local/bin/gpu_apply_ocs.sh > /dev/null <<'EOF'
-#!/bin/bash
-echo "Setting 5070Ti for mining... Keryx"
-
-py-nvtool --setcore 2100 --setcoreoffset 350 --setmem 0 --setmemoffset 1500
-
-EOF
-
-# make it executable
-sudo chmod +x /usr/local/bin/gpu_apply_ocs.sh
-
-# manual test
-sudo /usr/local/bin/gpu_apply_ocs.sh
-
